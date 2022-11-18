a5d5c78989b6166db5f21625a435dd57d6c2353e
kvcsPat <smth.patrik@gmail.com>
2022-11-18 13:00:51 +0100

    modified home page, added call to action button

--- cosmos/src/home/Page.jsx
+++ cosmos/src/home/Page.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Layout from "../routing/Layout";
 
 export default function HomePage() {
   return (
     <Layout>
-      <div>Home</div>
+      <div className="welcome-text">
+        <h1>Welcome to the page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
+          explicabo voluptatem beatae sequi adipisci quod, facilis, culpa fugit
+          ratione quisquam pariatur, corrupti nisi minima ducimus itaque autem
+          ipsam ad eveniet!
+        </p>
+      </div>
+      <NavLink to="/apod">
+        <button className="call-to-action">discover the cosmos</button>
+      </NavLink>
     </Layout>
   );
 }
