049cdc0dea15220acde70a83763163e527aa159d
kvcsPat <smth.patrik@gmail.com>
2022-11-18 13:30:31 +0100

    created custom fetch hook

---
+++ cosmos/src/hooks/useFetch.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+
+const cache = {};
+
+export default function useFetch(url) {
+  const initialState = {
+    status: "idle",
+    error: null,
+    data: null,
+  };
+
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "FETCHING":
+        return { ...initialState, status: "fetching" };
+      case "FETCHED":
+        return { ...initialState, status: "fetched", data: action.payload };
+      case "FETCH_ERROR":
+        return { ...initialState, status: "error", error: action.payload };
+      default:
+        return state;
+    }
+  }, initialState);
+
+  useEffect(() => {
+    let cancelRequest = false;
+    if (!url) return;
+
+    const fetchData = async () => {
+      dispatch({ type: "FETCHING" });
+      if (cache[url]) {
+        const data = cache[url];
+        dispatch({ type: "FETCHED", payload: data });
+      } else {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          cache[url] = data;
+          if (cancelRequest) return;
+          dispatch({ type: "FETCHED", payload: data });
+        } catch (error) {
+          if (cancelRequest) return;
+          dispatch({ type: "FETCH_ERROR", payload: error.message });
+        }
+      }
+    };
+
+    fetchData();
+
+    return function cleanup() {
+      cancelRequest = true;
+    };
+  }, [url]);
+
+  return state;
+}
