0e3a4c5b58892b4352b1fee1f25c605f8153fd33
kvcsPat <smth.patrik@gmail.com>
2023-01-06 11:58:36 +0100

    created css basics with sass

--- cosmos/src/App.js
+++ cosmos/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Routing from "./structure/routing/Routing";
+import "./styles_global/reset/reset.css";
+import "./styles_global/global.css";
 
 function App() {
-  return <Routing />;
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  );
 }
 
 export default App;
