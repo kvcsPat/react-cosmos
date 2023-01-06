a3e561a2f4fffb9aa2bbbd682ecd6ed9f842608e
kvcsPat <smth.patrik@gmail.com>
2023-01-06 15:24:23 +0100

    created navbar style, updated files accordingly

--- cosmos/src/structure/navbar/Navbar.jsx
+++ cosmos/src/structure/navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logoDark from "../../assets/logo_dark.svg";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to="/">home</NavLink>
-      <NavLink to="/apod">apod</NavLink>
-      <NavLink to="/search">search</NavLink>
-      <NavLink to="/gallery">gallery</NavLink>
-      <NavLink to="/about">about</NavLink>
+      <img className="logo" src={logoDark} alt="nasa_logo" />
+      <div className="nav_btns">
+        <NavLink className="nav_btn" to="/">
+          home
+        </NavLink>
+        <NavLink className="nav_btn" to="/apod">
+          apod
+        </NavLink>
+        <NavLink className="nav_btn" to="/search">
+          search
+        </NavLink>
+        <NavLink className="nav_btn" to="/gallery">
+          gallery
+        </NavLink>
+        <NavLink className="nav_btn" to="/about">
+          about
+        </NavLink>
+      </div>
     </nav>
   );
 }
