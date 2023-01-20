84c92cb9463f6a776bf04c12dad005266364e227
kvcsPat <smth.patrik@gmail.com>
2023-01-20 13:07:46 +0100

    refactored home page css

--- cosmos/src/structure/home/Component.jsx
+++ cosmos/src/structure/home/Component.jsx
@@ -4,17 +4,15 @@ import "./component.css";
 
 export default function HomeComponent() {
   return (
-    <section className="content">
-      <h1 className="welcome-title">Welcome to the page</h1>
-      <p className="welcome-text">
+    <section className="welcome-text">
+      <h1>Welcome to the page</h1>
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
         explicabo voluptatem beatae sequi adipisci quod, facilis, culpa fugit
         ratione quisquam pariatur, corrupti nisi minima ducimus itaque autem
         ipsam ad eveniet!
       </p>
-      <NavLink className="call-to-action" to="/apod">
-        discover the cosmos
-      </NavLink>
+      <NavLink to="/apod">discover the cosmos</NavLink>
     </section>
   );
 }
