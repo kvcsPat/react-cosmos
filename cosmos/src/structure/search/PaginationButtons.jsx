4d3bb04a6759c6587c10b424ae41cb651a96f0bb
kvcsPat <smth.patrik@gmail.com>
2023-02-18 12:39:22 +0100

    created disabled style for pagination buttons

--- cosmos/src/structure/search/PaginationButtons.jsx
+++ cosmos/src/structure/search/PaginationButtons.jsx
@@ -1,12 +1,51 @@
 import React from "react";
+import { firstDate, currentDate } from "../../utils/dates";
+import { formatDate } from "../../utils/formatDate";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function PaginationButtons({ handlePrevious, handleNext }) {
+  const params = useParams();
+
+  const [isDisabled, setIsDisabled] = useState({
+    isFirstDate: false,
+    isCurrentDate: false,
+  });
+
+  useEffect(() => {
+    if (params.value === formatDate(firstDate)) {
+      setIsDisabled((prevState) => {
+        return { ...prevState, isFirstDate: true };
+      });
+    }
+    if (params.value === formatDate(currentDate)) {
+      setIsDisabled((prevState) => {
+        return { ...prevState, isCurrentDate: true };
+      });
+    }
+    if (
+      params.value !== formatDate(firstDate) &&
+      params.value !== formatDate(currentDate)
+    ) {
+      setIsDisabled(() => {
+        return { isFirstDate: false, isCurrentDate: false };
+      });
+    }
+  }, [params.value]);
+
   return (
     <div className="apod-btns">
-      <button className="previous-btn" onClick={handlePrevious}>
+      <button
+        className={`previous-btn ${isDisabled.isFirstDate ? "disabled" : ""}`}
+        onClick={handlePrevious}
+      >
         previous day
       </button>
-      <button className="next-btn" onClick={handleNext}>
+      <button
+        className={`previous-btn ${isDisabled.isCurrentDate ? "disabled" : ""}`}
+        onClick={handleNext}
+      >
         next day
       </button>
     </div>
