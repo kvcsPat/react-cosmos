aec532c82182252ee95ecd669b9708174406265c
kvcsPat <smth.patrik@gmail.com>
2023-02-18 17:52:18 +0100

    corrected solar system animation

--- cosmos/src/structure/home/SolarSystem.jsx
+++ cosmos/src/structure/home/SolarSystem.jsx
@@ -5,29 +5,29 @@ export default function SolarSystem() {
   return (
     <div className="solar-system">
       <div className="sun"></div>
-      <div className="mercury-outline">
+      <div className="mercury-orbit">
         <div className="mercury"></div>
       </div>
-      <div className="venus-outline">
+      <div className="venus-orbit">
         <div className="venus"></div>
       </div>
-      <div className="earth-outline">
+      <div className="earth-orbit">
         <div className="earth"></div>
       </div>
-      <div className="mars-outline">
+      <div className="mars-orbit">
         <div className="mars"></div>
       </div>
-      <div className="jupiter-outline">
+      <div className="jupiter-orbit">
         <div className="jupiter"></div>
       </div>
-      <div className="saturn-outline">
+      <div className="saturn-orbit">
         <div className="saturn"></div>
         <div className="ring"></div>
       </div>
-      <div className="uranus-outline">
+      <div className="uranus-orbit">
         <div className="uranus"></div>
       </div>
-      <div className="neptune-outline">
+      <div className="neptune-orbit">
         <div className="neptune"></div>
       </div>
     </div>
